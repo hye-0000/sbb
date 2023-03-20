21184e30e5bd5366e3c09f9d1dbb8ada6b9d38b1
hye-0000 <hye_0000@naver.com>
2023-03-20 16:35:55 +0900

    test 코드 실행 안됨

---
+++ src/main/java/com/mysite/sbb/entity/AnswerForm.java
@@ -0,0 +1,12 @@
+package com.mysite.sbb.entity;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AnswerForm {
+    @NotEmpty(message = "내용은 필수 항목입ㄴ디ㅏ.")
+    private String content;
+}
